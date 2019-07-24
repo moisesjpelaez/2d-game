b5a3baea84f2f1a888b9229bde7778efc6040025
moisesjpelaez <moises.jpelaez@gmail.com>
2019-07-24 19:40:52 -0400

    One song removed due possible licensing issues.

--- Scripts/GameUI.gd
+++ Scripts/GameUI.gd
@@ -1,6 +1,7 @@
 extends CanvasLayer
 
 export (Texture) var Heart
+var total_music = 5
 
 func _ready():
 	Global.game_label = $Control/Label
@@ -8,7 +9,7 @@ func _ready():
 	Global.ui_health = $Control/Health
 	Global.heart_texture = Heart
 	randomize()
-	Global.game_music = get_node("Music" + str(randi() % 6 + 1))
+	Global.game_music = get_node("Music" + str(randi() % total_music + 1))
 	
 	$Control/Health.hide()
 	$Control/Label.text = "press the spacebar to start\n press 'esc' to quit"
@@ -21,7 +22,7 @@ func _ready():
 	if Global.game_started:
 		Global.start_game()
 
-	for i in range(1,7):
+	for i in range(1, total_music + 1):
 		get_node("Music" + str(i)).connect("finished", self, "change_music")
 
 func _on_RestartTimer_timeout():
@@ -29,5 +30,5 @@ func _on_RestartTimer_timeout():
 
 func change_music():
 	randomize()
-	Global.game_music = get_node("Music" + str(randi() % 6 + 1))
+	Global.game_music = get_node("Music" + str(randi() % total_music + 1))
 	Global.game_music.play()
