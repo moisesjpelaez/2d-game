62fde9a3023a2c4c109f6686fc10b290f13acff0
moisesjpelaez <moises.jpelaez@gmail.com>
2020-06-18 11:20:51 -0400

    Minor changes. Ignored music folder.

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -19,31 +19,31 @@ signal start_game
 signal shake_screen(amount, duration)
 
 func start_game():
-    player_lives = 2
-    game_label.hide()
-    ui_health.show()
-    emit_signal("start_game")
-    game_music.play()
-    game_music.get_node("AnimationPlayer").play("FadeIn")
+	player_lives = 2
+	game_label.hide()
+	ui_health.show()
+	emit_signal("start_game")
+	game_music.play()
+	game_music.get_node("AnimationPlayer").play("FadeIn")
 
 func game_over():
-    game_is_over = true
-    game_label.show()
-    ui_health.hide()
-    game_label.text = game_over_text
-    restart_timer.start()
-    game_music.get_node("AnimationPlayer").play("FadeOut")
+	game_is_over = true
+	game_label.show()
+	ui_health.hide()
+	game_label.text = game_over_text
+	restart_timer.start()
+	game_music.get_node("AnimationPlayer").play("FadeOut")
 
 func show_restart():
-    game_label.text = restart_text
-    can_restart = true
+	game_label.text = restart_text
+	can_restart = true
 
 func _input(event):
-    if event.is_action_pressed("jump") && !game_started:
-        game_started = true
-        game_is_over = false
-        start_game()
-    if event.is_action_pressed("jump") && can_restart:
-        get_tree().reload_current_scene()
-    if event.is_action_pressed("quit"):
-        get_tree().quit()
+	if event.is_action_pressed("jump") && !game_started:
+		game_started = true
+		game_is_over = false
+		start_game()
+	if event.is_action_pressed("jump") && can_restart:
+		get_tree().reload_current_scene()
+	if event.is_action_pressed("quit"):
+		get_tree().quit()
